87c08e023b6cd241855a6cacac70a5deb6e20231
hongquy0802 <hongquy0802@gmail.com>
2021-08-13 10:12:19 +0700

    Exercise 01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SimpleRedditComponent } from './simple-reddit/simple-reddit.component';
+import { ArticleComponent } from './article/article.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @NgModule({
   declarations: [
     AppComponent,
+    SimpleRedditComponent,
+    ArticleComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/simple-reddit/simple-reddit.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { RedditService } from '../services/reddit.service';
+
+@Component({
+  selector: 'app-simple-reddit',
+  templateUrl: './simple-reddit.component.html',
+  styleUrls: ['./simple-reddit.component.css']
+})
+export class SimpleRedditComponent implements OnInit {
+
+  constructor(private redditSer: RedditService) { }
+
+  ngOnInit(): void {
+  }
+
+  submitMethod(refForm: any) {
+   console.log(refForm);
+   this.redditSer.addValueLinkFrom(refForm.form.value);
+  }
+
+}
